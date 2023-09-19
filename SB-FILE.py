5a18cfa093122e379cd601ec79ce50addffe19b7
bipul159 <115370450+bipul159@users.noreply.github.com>
2023-09-19 09:19:30 +0600

    Add files via upload

---
+++ SB-FILE.py
@@ -0,0 +1,23 @@
+#WRITTEN BY BIPUL KHAN
+#FOLLOW MY GITHUB : https//github.com/bipul159
+#-----------import-----------#
+import os
+#-----------logo-------------#
+logo=("""
+888888 888888    
+ 8888888`888 88 88        
+ 88 88     88 88        
+ 88   `8. 88  88        88     88 88        
+ 88    .88 88  88       .   .8 88        
+ 88888888 88888888
+#----------clear-----------#
+def clear():
+	 os.system(clear)
+	 print(logo)
+	 print(42*'-')
+	 print(' FB ID : Bipul Khan')
+	 print(' GITHUB : bipul159')
+	 print(42*'-')
+#-----------menu------------#
+#-----------end-------------#
+clear()
